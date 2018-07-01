376600b3beb0539df4801cb5389b4a8103f3dc12
zehfernandes <ozehfernandes@gmail.com>
2018-07-01 17:52:42 -0300

    new match

--- webpack.config.js
+++ webpack.config.js
@@ -48,7 +48,7 @@ module.exports = {
                     drop_debugger: true,
                     conditionals: true,
                     evaluate: true,
-                    drop_console: true, // strips console statements
+                    //drop_console: true, // strips console statements
                     sequences: true,
                     booleans: true
                 }
